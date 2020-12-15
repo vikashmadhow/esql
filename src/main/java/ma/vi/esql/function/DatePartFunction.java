2b99512d7fd69dc8403a27485d9a99c6317983cc
vikash.madhow@gmail.com <vikash.madhow@gmail.com>
2020-12-15 07:47:56 -0800

    Cleanup

--- src/main/java/ma/vi/esql/function/DatePartFunction.java
+++ src/main/java/ma/vi/esql/function/DatePartFunction.java
@@ -29,19 +29,19 @@ public class DatePartFunction extends Function {
   }
 
   public enum Part {
-    Year("year", "year"),
-    Quarter("quarter", "quarter"),
-    Semester("semester", "semester"),
-    Month("month", "month"),
-    DayOfYear("doy", "dayofyear"),
-    Day("day", "day"),
-    Week("week", "week"),
-    DayOfWeek("dow", "weekday"),
-    Hour("hour", "hour"),
-    Minute("minute", "minute"),
-    Second("second", "second"),
-    Millisecond("milliseconds", "millisecond"),
-    Microsecond("microseconds", "microsecond");
+    Year        ("year",          "year"),
+    Quarter     ("quarter",       "quarter"),
+    Semester    ("semester",      "semester"),
+    Month       ("month",         "month"),
+    DayOfYear   ("doy",           "dayofyear"),
+    Day         ("day",           "day"),
+    Week        ("week",          "week"),
+    DayOfWeek   ("dow",           "weekday"),
+    Hour        ("hour",          "hour"),
+    Minute      ("minute",        "minute"),
+    Second      ("second",        "second"),
+    Millisecond ("milliseconds",  "millisecond"),
+    Microsecond ("microseconds",  "microsecond");
 
     public final String postgresqlName;
     public final String mssqlName;
@@ -59,52 +59,52 @@ public class DatePartFunction extends Function {
     if (target == POSTGRESQL) {
       if (part == Semester) {
         return "case date_part('" + Quarter.postgresqlName + "', " + arg + ") "
-            + "when 1 then 1 "
-            + "when 2 then 1 "
-            + "when 3 then 2 "
-            + "when 4 then 2 "
-            + "end";
+             + "  when 1 then 1 "
+             + "  when 2 then 1 "
+             + "  when 3 then 2 "
+             + "  when 4 then 2 "
+             + "end";
       } else {
         return "date_part('" + part.postgresqlName + "', " + arg + ')';
       }
     } else if (target == SQLSERVER) {
       if (part == Semester) {
         return "case datepart(" + Quarter.mssqlName + ", " + arg + ") "
-            + "when 1 then 1 "
-            + "when 2 then 1 "
-            + "when 3 then 2 "
-            + "when 4 then 2 "
-            + "end";
+             + "  when 1 then 1 "
+             + "  when 2 then 1 "
+             + "  when 3 then 2 "
+             + "  when 4 then 2 "
+             + "end";
       } else if (part == Millisecond) {
         // SQL Server does not include seconds in milliseconds: include it as it is
         // more sensible and similar to postgresql
-        return "datepart(" + Second.mssqlName + ", " + arg + ") * 1000 + " +
-            "datepart(" + part.mssqlName + ", " + arg + ')';
+        return "datepart(" + Second.mssqlName + ", " + arg + ") * 1000 + "
+             + "datepart(" + part.mssqlName + ", " + arg + ')';
 
       } else if (part == Microsecond) {
         // SQL Server does not include seconds in microseconds: include it as
         // it is more sensible and similar to postgresql
-        return "datepart(" + Second.mssqlName + ", " + arg + ") * 1000000 + " +
-            "datepart(" + part.mssqlName + ", " + arg + ')';
+        return "datepart(" + Second.mssqlName + ", " + arg + ") * 1000000 + "
+             + "datepart(" + part.mssqlName + ", " + arg + ')';
 
       } else {
         return "datepart(" + part.mssqlName + ", " + arg + ')';
       }
     } else if (target == JAVASCRIPT) {
       return switch (part) {
-        case Year -> "(" + arg + ").year()";
-        case Quarter -> "(" + arg + ").quarter()";
-        case Semester -> "((" + arg + ").quarter()==1 || (" + arg + ").quarter()==2 ? 1 : 2)";
-        case Month -> "((" + arg + ").month() + 1)";
-        case Week -> "(" + arg + ").week()";
-        case DayOfWeek -> "((" + arg + ").dayOfWeek() + 1)";
-        case DayOfYear -> "(" + arg + ").dayOfYear()";
-        case Day -> "(" + arg + ").date()";
-        case Hour -> "(" + arg + ").hour()";
-        case Minute -> "(" + arg + ").minute()";
-        case Second -> "(" + arg + ").second()";
-        case Millisecond -> "(" + arg + ").millisecond()";
-        case Microsecond -> "((" + arg + ").millisecond() * 1000)";
+        case Year         -> "(" + arg + ").year()";
+        case Quarter      -> "(" + arg + ").quarter()";
+        case Semester     -> "((" + arg + ").quarter()==1 || (" + arg + ").quarter()==2 ? 1 : 2)";
+        case Month        -> "((" + arg + ").month() + 1)";
+        case Week         -> "(" + arg + ").week()";
+        case DayOfWeek    -> "((" + arg + ").dayOfWeek() + 1)";
+        case DayOfYear    -> "(" + arg + ").dayOfYear()";
+        case Day          -> "(" + arg + ").date()";
+        case Hour         -> "(" + arg + ").hour()";
+        case Minute       -> "(" + arg + ").minute()";
+        case Second       -> "(" + arg + ").second()";
+        case Millisecond  -> "(" + arg + ").millisecond()";
+        case Microsecond  -> "((" + arg + ").millisecond() * 1000)";
       };
     } else {
       return name + '(' + arg + ')';
