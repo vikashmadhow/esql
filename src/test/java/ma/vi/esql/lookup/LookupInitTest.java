45ef62f53d18002e88ce7c8177423296edc1876a
Vikash Madhow <vikash.madhow@gmail.com>
2024-03-09 23:50:03 -0500

    Base Lookup initializer

---
+++ src/test/java/ma/vi/esql/lookup/LookupInitTest.java
@@ -0,0 +1,50 @@
+package ma.vi.esql.lookup;
+
+import ma.vi.esql.database.EsqlConnection;
+import ma.vi.esql.database.init.lookup.LookupInitializer;
+import ma.vi.esql.extension.lookup.Lookup;
+import ma.vi.esql.extension.lookup.LookupExtension;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+/**
+ * @author Vikash Madhow (vikash.madhow@gmail.com)
+ */
+class LookupInitTest extends DataTest {
+  @TestFactory
+  Stream<DynamicTest> initLookups() {
+    return Stream.of(databases)
+                 .map(db -> dynamicTest(db.target().toString(), () -> {
+                   System.out.println(db.target());
+                   try (EsqlConnection con = db.esql()) {
+                       con.exec("delete r from r:_core.resource");
+                   }
+                   new LookupInitializer().add(db, "/init/test_lookups.yml", LookupInitTest.class.getResourceAsStream("/init/test_lookups.yml"));
+
+                   LookupExtension ext = db.extension(LookupExtension.class);
+                   Lookup lookup = ext.loadLookup("TestCountry");
+                   assertEquals("TestCountry", lookup.name());
+                   assertEquals("Country", lookup.displayName());
+                   assertEquals("Countries", lookup.description());
+                   assertEquals("Geography", lookup.group());
+
+                   assertEquals(2, lookup.links().size());
+                   Map<String, Lookup> linkMap = lookup.links().stream()
+                                                       .collect(Collectors.toMap(Lookup::name, l -> l));
+
+                   assertEquals(ext.loadLookup("TestCurrency"),
+                                linkMap.get("TestCurrency"));
+                   assertEquals(ext.loadLookup("TestRegionalGrouping"),
+                                linkMap.get("TestRegionalGrouping"));
+
+                   System.out.println(lookup.values().values());
+                 }));
+  }
+}
